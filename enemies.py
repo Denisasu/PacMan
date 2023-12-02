2bd3da73f65247654bf059514102c7bb2a29e9ba
dut0va <148580644+dut0va@users.noreply.github.com>
2023-12-02 19:28:39 +0700

    Update enemies.py

--- enemies.py
+++ enemies.py
@@ -4,7 +4,7 @@ import random
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 576
 
-# Define some colors
+#определяем цвета
 BLACK = (0,0,0)
 WHITE = (255,255,255)
 BLUE = (0,0,255)
@@ -13,9 +13,9 @@ RED = (255,0,0)
 
 class Block(pygame.sprite.Sprite):
     def __init__(self,x,y,color,width,height):
-        # Call the parent class (Sprite) constructor
+        #вызов конструктора родительского класса (sprite)
         pygame.sprite.Sprite.__init__(self)
-        # Set the background color and set it to be transparent
+        #прозрачный цвет фона
         self.image = pygame.Surface([width,height])
         self.image.fill(color)
         self.rect = self.image.get_rect()
@@ -24,30 +24,27 @@ class Block(pygame.sprite.Sprite):
 
 class Ellipse(pygame.sprite.Sprite):
     def __init__(self,x,y,color,width,height):
-        # Call the parent class (Sprite) constructor
         pygame.sprite.Sprite.__init__(self)
-        # Set the background color and set it to be transparent
         self.image = pygame.Surface([width,height])
         self.image.fill(BLACK)
         self.image.set_colorkey(BLACK)
-        # Draw the ellipse
+        #рисуем эллипс
         pygame.draw.ellipse(self.image,color,[0,0,width,height])
         self.rect = self.image.get_rect()
         self.rect.topleft = (x,y)
 
-        
+#Задаем класс Slime, наследующийся от класса pygame.sprite.Sprite.       
 class Slime(pygame.sprite.Sprite):
     def __init__(self,x,y,change_x,change_y):
-        # Call the parent class (Sprite) constructor
         pygame.sprite.Sprite.__init__(self)
-        # Set the direction of the slime
+        #задаем направление противнику
         self.change_x = change_x
         self.change_y = change_y
-        # Load image
+        #загрузка изображения
         self.image = pygame.image.load("slime1.png").convert_alpha()
         self.rect = self.image.get_rect()
         self.rect.topleft = (x,y)
-    
+    #метод update принимает горизонтальные и вертикальные стены
     def update(self,horizontal_blocks,vertical_blocks):
         self.rect.x += self.change_x
         self.rect.y += self.change_y
@@ -59,7 +56,7 @@ class Slime(pygame.sprite.Sprite):
             self.rect.top = SCREEN_HEIGHT
         elif self.rect.top > SCREEN_HEIGHT:
             self.rect.bottom = 0
-
+     #позиции
         if self.rect.topleft in self.get_intersection_position():
             direction = random.choice(("left","right","up","down"))
             if direction == "left" and self.change_x == 0:
@@ -82,9 +79,9 @@ class Slime(pygame.sprite.Sprite):
                 if item == 3:
                     items.append((j*32,i*32))
 
-        return items 
+        return items #возвращаем список позиций
     
-
+#Задаем класс Ghost, наследующийся от класса pygame.sprite.Sprite.  
 class Ghost(pygame.sprite.Sprite):
     def __init__(self, x, y, change_x, change_y):
         # Call the parent class (Sprite) constructor
@@ -132,7 +129,8 @@ class Ghost(pygame.sprite.Sprite):
                     items.append((j*32,i*32))
 
         return items
-
+        
+#Задаем класс Specter, наследующийся от класса pygame.sprite.Sprite.  
 class Specter(pygame.sprite.Sprite):
     def __init__(self, x, y, change_x, change_y):
         # Call the parent class (Sprite) constructor
@@ -180,7 +178,8 @@ class Specter(pygame.sprite.Sprite):
                     items.append((j*32,i*32))
 
         return items
-
+        
+#Задаем класс Phantom, наследующийся от класса pygame.sprite.Sprite.  
 class Phantom(pygame.sprite.Sprite):
     def __init__(self, x, y, change_x, change_y):
         # Call the parent class (Sprite) constructor
@@ -229,7 +228,7 @@ class Phantom(pygame.sprite.Sprite):
 
         return items
     
-        
+#позиционирование
 def enviroment():
     grid = ((0,2,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,2,0,0,0,0,0,2,0),
             (0,2,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,2,0,0,0,0,0,2,0),
@@ -251,7 +250,7 @@ def enviroment():
             (0,2,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,2,0,0,0,0,0,2,0))
 
     return grid
-
+#отрисовка стен
 def draw_enviroment(screen):
     for i,row in enumerate(enviroment()):
         for j,item in enumerate(row):
